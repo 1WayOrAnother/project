f31168f65df18d00b7297d4718ce89e92be33218
1WayOrAnother <youneedit2@gmail.com>
2017-12-01 14:54:30 -0800

    this is updating scrits.js this will add submit function etc..

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function){
+  $("trackform").submt(function(event)
+  event.preventDefault();
+
+  var question
+  var question
+  var question
+  var question
+  var question
+  var question
+  var question
+  
+}
